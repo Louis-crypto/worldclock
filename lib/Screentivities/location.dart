45b0bd010533582a7db1fc8196780502c766ee2b
Louis-crypto <kinglouis745@gmail.com>
2022-04-26 17:53:30 +0300

    add flags for different locations

--- lib/Screentivities/location.dart
+++ lib/Screentivities/location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/worldtime.dart';
 
 class Location extends StatefulWidget {
 
@@ -7,6 +8,20 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+
+  List<WorldTime> locations = [
+    WorldTime('Accra', 'flag', 'Africa/Accra'),
+    WorldTime('Chicago', 'flag', 'America/Chicago'),
+    WorldTime('Denver', 'flag', 'America/Denver'),
+    WorldTime('Dubai', 'flag', 'Asia/Dubai'),
+    WorldTime('London', 'flag', 'Europe/London'),
+    WorldTime('Mauritius', 'flag', 'Indian/Mauritius'),
+    WorldTime('Nairobi', 'flag', 'Africa/Nairobi'),
+    WorldTime('Rome', 'flag', 'Europe/Rome'),
+    WorldTime('Shanghai', 'flag', 'Asia/Shanghai'),
+    WorldTime('Sydney', 'flag', 'Australia/Sydney'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
